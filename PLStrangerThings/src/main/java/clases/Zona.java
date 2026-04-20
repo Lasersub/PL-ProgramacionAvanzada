27a8826da157c6b8e3fa4c66fcec6ecd1cfac493
RayanBZaki <rayan.bentaleb@edu.uah.es>
2026-04-20 12:59:06 +0200

    Clase Zona implementada
    Listas de hilos activos implementadas como CopyOnWriteArrayList, añadido ReentrantLock y Conditions para la entrada y salida de la zona. Tambien manejo de interrupciones con esperarFinAtaque

--- PLStrangerThings/src/main/java/clases/Zona.java
+++ PLStrangerThings/src/main/java/clases/Zona.java
@@ -4,10 +4,87 @@
  */
 package clases;
 
+import java.util.List;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+
 /**
  *
  * @author User
  */
 public class Zona {
     
+    private List listaNinos; //CopyOnWriteArrayList
+    private List listaDemogorgons; //CopyOnWriteArrayList
+    private Lock cerrojo; //Reentrar Lock
+    private Condition condicionFinAtaque;
+    
+    public void entrarNino(Nino nino){
+        cerrojo.lock();
+        try{
+            listaNinos.add(nino);
+        }
+        catch(Exception e){
+            
+        }
+        finally{
+            cerrojo.unlock();
+        }
+        
+    }
+    
+    public void entrarDemogorgon(Demogorgon demog){
+        cerrojo.lock();
+        try{
+            listaDemogorgons.add(demog);
+        }
+        catch(Exception e){
+            
+        }
+        finally{
+            cerrojo.unlock();
+        }
+    }
+    
+    public void salirNino(Nino nino){
+        cerrojo.lock();
+        try{
+            listaNinos.remove(nino);
+        }
+        catch(Exception e){
+            
+        }
+        finally{
+            cerrojo.unlock();
+        }
+        
+    }
+    
+    public void salirDemogorgon(Demogorgon demog){
+        cerrojo.lock();
+        try{
+            listaDemogorgons.remove(demog);
+        }
+        catch(Exception e){
+            
+        }
+        finally{
+            cerrojo.unlock();
+        }
+    }
+    
+    public void esperarFinAtaque(Nino nino){
+        cerrojo.lock();
+        try{
+            while(nino.isSiendoAtacado() == true){
+                condicionFinAtaque.await();
+            }
+        }
+        catch(InterruptedException e){
+            System.out.println("Niño interrumpido"); //Manejacion de error temporal
+        }
+        finally{
+            cerrojo.unlock();
+        }
+    }
 }
