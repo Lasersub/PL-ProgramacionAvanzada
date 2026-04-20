72d9171378df5f1a86f9c029b5dbf0835bdfbaac
Óscar López Rojo <oscar.lopezrojo@edu.uah.es>
2026-04-20 13:05:21 +0200

    Portada completada

--- PLStrangerThings/src/main/java/interfaces/PanelPortada.java
+++ PLStrangerThings/src/main/java/interfaces/PanelPortada.java
@@ -26,19 +26,25 @@ public class PanelPortada extends javax.swing.JPanel {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
-        this.setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
-        );
+        jLabel1 = new javax.swing.JLabel();
+        imagenPortada = new javax.swing.JLabel();
+
+        setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jLabel1.setFont(new java.awt.Font("Yatra One", 1, 48)); // NOI18N
+        jLabel1.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel1.setText("SIMULACIÓN INICIADA");
+        add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 1200, 420));
+
+        imagenPortada.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/portada.png"))); // NOI18N
+        imagenPortada.setText("P");
+        add(imagenPortada, new org.netbeans.lib.awtextra.AbsoluteConstraints(-40, 0, 1270, 580));
     }// </editor-fold>//GEN-END:initComponents
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel imagenPortada;
+    private javax.swing.JLabel jLabel1;
     // End of variables declaration//GEN-END:variables
 }
