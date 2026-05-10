e6c80ccb211889b71f6ac58397d3803fb67cb86c
Óscar López Rojo <oscar.lopezrojo@edu.uah.es>
2026-05-10 12:07:12 +0200

    Ampliadas clases RMI para soportar PanelInfo.java

--- PLStrangerThings/src/main/java/clases/ISimulacionRemota.java
+++ PLStrangerThings/src/main/java/clases/ISimulacionRemota.java
@@ -1,24 +1,58 @@
 package clases;
 
 public interface ISimulacionRemota extends java.rmi.Remote {
-
-    int getNinosEnHawkins() throws java.rmi.RemoteException;
-    int getNinosPortalBosque() throws java.rmi.RemoteException;
-    int getNinosPortalLaboratorio() throws java.rmi.RemoteException;
-    int getNinosPortalCentroComercial() throws java.rmi.RemoteException;
-    int getNinosPortalAlcantarillado() throws java.rmi.RemoteException;
-    int getNinosLaboratorio() throws java.rmi.RemoteException;
-    int getNinosCentroComercial() throws java.rmi.RemoteException;
-    int getNinosBosque() throws java.rmi.RemoteException;
-    int getNinosAlcantarillado() throws java.rmi.RemoteException;
-    int getNinosColmena() throws java.rmi.RemoteException;
-    int getDemogorgonesLaboratorio() throws java.rmi.RemoteException;
-    int getDemogorgonesCentroComercial() throws java.rmi.RemoteException;
-    int getDemogorgonesBosque() throws java.rmi.RemoteException;
-    int getDemogorgonesAlcantarillado() throws java.rmi.RemoteException;
+    // Interfaz PanelControl.java
+    int getNumNinosEnHawkins() throws java.rmi.RemoteException;
+    
+    int getNumNinosPortalBosque() throws java.rmi.RemoteException;
+    int getNumNinosPortalLaboratorio() throws java.rmi.RemoteException;
+    int getNumNinosPortalCentroComercial() throws java.rmi.RemoteException;
+    int getNumNinosPortalAlcantarillado() throws java.rmi.RemoteException;
+    
+    int getNumNinosLaboratorio() throws java.rmi.RemoteException;
+    int getNumNinosCentroComercial() throws java.rmi.RemoteException;
+    int getNumNinosBosque() throws java.rmi.RemoteException;
+    int getNumNinosAlcantarillado() throws java.rmi.RemoteException;
+    int getNumNinosColmena() throws java.rmi.RemoteException;
+    
+    int getNumDemogorgonesLaboratorio() throws java.rmi.RemoteException;
+    int getNumDemogorgonesCentroComercial() throws java.rmi.RemoteException;
+    int getNumDemogorgonesBosque() throws java.rmi.RemoteException;
+    int getNumDemogorgonesAlcantarillado() throws java.rmi.RemoteException;
+    
     java.util.List<String> getRankingTop3() throws java.rmi.RemoteException;
+    
     String getEventoActivo() throws java.rmi.RemoteException;
     long getSegundosRestantesEvento() throws java.rmi.RemoteException;
+    
     void pausarReanudar() throws java.rmi.RemoteException;
     boolean isPausado() throws java.rmi.RemoteException;
+    
+    
+    
+    // Interfaz PanelInfo.java
+    java.util.List<Nino> getNinosCallePrincipal() throws java.rmi.RemoteException;
+    java.util.List<Nino> getNinosSotanoByers() throws java.rmi.RemoteException;
+    java.util.List<Nino> getNinosRadioWSQK() throws java.rmi.RemoteException;
+    
+    java.util.List<Nino> getNinosLaboratorio() throws java.rmi.RemoteException;
+    java.util.List<Nino> getNinosCentroComercial() throws java.rmi.RemoteException;
+    java.util.List<Nino> getNinosBosque() throws java.rmi.RemoteException;
+    java.util.List<Nino> getNinosAlcantarillado() throws java.rmi.RemoteException;
+    
+    java.util.List<Nino> getNinosPortalIdaLaboratorio() throws java.rmi.RemoteException;
+    java.util.List<Nino> getNinosPortalIdaCentroComercial() throws java.rmi.RemoteException;
+    java.util.List<Nino> getNinosPortalIdaBosque() throws java.rmi.RemoteException;
+    java.util.List<Nino> getNinosPortalIdaAlcantarillado() throws java.rmi.RemoteException;
+    
+    int getNinosPortalVueltaLaboratorio() throws java.rmi.RemoteException;
+    int getNinosPortalVueltaCentroComercial() throws java.rmi.RemoteException;
+    int getNinosPortalVueltaBosque() throws java.rmi.RemoteException;
+    int getNinosPortalVueltaAlcantarillado() throws java.rmi.RemoteException;
+    
+    java.util.List<Demogorgon> getDemogorgonsLaboratorio() throws java.rmi.RemoteException;
+    java.util.List<Demogorgon> getDemogorgonsCentroComercial() throws java.rmi.RemoteException;
+    java.util.List<Demogorgon> getDemogorgonsBosque() throws java.rmi.RemoteException;
+    java.util.List<Demogorgon> getDemogorgonsAlcantarillado() throws java.rmi.RemoteException;
+    
 }
--- PLStrangerThings/src/main/java/clases/SimulacionRemota.java
+++ PLStrangerThings/src/main/java/clases/SimulacionRemota.java
@@ -16,78 +16,78 @@ public class SimulacionRemota extends UnicastRemoteObject implements ISimulacion
     }
 
     @Override
-    public int getNinosEnHawkins() throws RemoteException {
+    public int getNumNinosEnHawkins() throws RemoteException {
         return backend.getHawkins().getCallePrincipal().getListaNinos().size()
              + backend.getHawkins().getSotanoByers().getListaNinos().size()
              + backend.getHawkins().getRadioWSQK().getListaNinos().size();
     }
 
     @Override
-    public int getNinosPortalBosque() throws RemoteException {
+    public int getNumNinosPortalBosque() throws RemoteException {
         Portal p = backend.getHawkins().getPortalBosque();
         return p.getNinosEnColaIda().size() + p.getNinosEsperandoVuelta();
     }
 
     @Override
-    public int getNinosPortalLaboratorio() throws RemoteException {
+    public int getNumNinosPortalLaboratorio() throws RemoteException {
         Portal p = backend.getHawkins().getPortalLaboratorio();
         return p.getNinosEnColaIda().size() + p.getNinosEsperandoVuelta();
     }
 
     @Override
-    public int getNinosPortalCentroComercial() throws RemoteException {
+    public int getNumNinosPortalCentroComercial() throws RemoteException {
         Portal p = backend.getHawkins().getPortalCentroComercial();
         return p.getNinosEnColaIda().size() + p.getNinosEsperandoVuelta();
     }
 
     @Override
-    public int getNinosPortalAlcantarillado() throws RemoteException {
+    public int getNumNinosPortalAlcantarillado() throws RemoteException {
         Portal p = backend.getHawkins().getPortalAlcantarillado();
         return p.getNinosEnColaIda().size() + p.getNinosEsperandoVuelta();
     }
 
     @Override
-    public int getNinosLaboratorio() throws RemoteException {
+    public int getNumNinosLaboratorio() throws RemoteException {
         return backend.getUpsideDown().getLaboratorio().getListaNinos().size();
     }
 
     @Override
-    public int getNinosCentroComercial() throws RemoteException {
+    public int getNumNinosCentroComercial() throws RemoteException {
         return backend.getUpsideDown().getCentroComercial().getListaNinos().size();
     }
 
     @Override
-    public int getNinosBosque() throws RemoteException {
+    public int getNumNinosBosque() throws RemoteException {
         return backend.getUpsideDown().getBosque().getListaNinos().size();
     }
 
     @Override
-    public int getNinosAlcantarillado() throws RemoteException {
+    public int getNumNinosAlcantarillado() throws RemoteException {
         return backend.getUpsideDown().getAlcantarillado().getListaNinos().size();
     }
 
     @Override
-    public int getNinosColmena() throws RemoteException {
+    public int getNumNinosColmena() throws RemoteException {
         return backend.getUpsideDown().getColmena().getListaNinos().size();
     }
 
     @Override
-    public int getDemogorgonesLaboratorio() throws RemoteException {
+    public int getNumDemogorgonesLaboratorio() throws RemoteException {
         return backend.getUpsideDown().getLaboratorio().getListaDemogorgons().size();
     }
 
     @Override
-    public int getDemogorgonesCentroComercial() throws RemoteException {
+    public int getNumDemogorgonesCentroComercial() throws RemoteException {
         return backend.getUpsideDown().getCentroComercial().getListaDemogorgons().size();
     }
 
     @Override
-    public int getDemogorgonesBosque() throws RemoteException {
+    public int getNumDemogorgonesBosque() throws RemoteException {
         return backend.getUpsideDown().getBosque().getListaDemogorgons().size();
     }
 
     @Override
-    public int getDemogorgonesAlcantarillado() throws RemoteException {
+    public int getNumDemogorgonesAlcantarillado() throws RemoteException {
         return backend.getUpsideDown().getAlcantarillado().getListaDemogorgons().size();
     }
 
@@ -131,5 +131,113 @@ public class SimulacionRemota extends UnicastRemoteObject implements ISimulacion
     @Override
     public boolean isPausado() throws RemoteException {
         return backend.isPausado();
+    }
+
+    
+    
+    
+    
+    
+    @Override
+    public List<Nino> getNinosLaboratorio() throws RemoteException {
+        return backend.getUpsideDown().getLaboratorio().getListaNinos();
+    }
+
+    @Override
+    public List<Nino> getNinosCentroComercial() throws RemoteException {
+        return backend.getUpsideDown().getCentroComercial().getListaNinos();
+    }
+
+    @Override
+    public List<Nino> getNinosBosque() throws RemoteException {
+        return backend.getUpsideDown().getBosque().getListaNinos();
+    }
+
+    @Override
+    public List<Nino> getNinosAlcantarillado() throws RemoteException {
+        return backend.getUpsideDown().getAlcantarillado().getListaNinos();
+    }
+
+    @Override
+    public List<Nino> getNinosPortalIdaLaboratorio() throws RemoteException {
+        Portal p = backend.getHawkins().getPortalLaboratorio();
+        return p.getNinosEnColaIda();
+    }
+
+    @Override
+    public List<Nino> getNinosPortalIdaCentroComercial() throws RemoteException {
+        Portal p = backend.getHawkins().getPortalCentroComercial();
+        return p.getNinosEnColaIda();
+    }
+
+    @Override
+    public List<Nino> getNinosPortalIdaBosque() throws RemoteException {
+        Portal p = backend.getHawkins().getPortalBosque();
+        return p.getNinosEnColaIda();
+    }
+
+    @Override
+    public List<Nino> getNinosPortalIdaAlcantarillado() throws RemoteException {
+        Portal p = backend.getHawkins().getPortalAlcantarillado();
+        return p.getNinosEnColaIda();
+    }
+
+    @Override
+    public int getNinosPortalVueltaLaboratorio() throws RemoteException {
+        Portal p = backend.getHawkins().getPortalLaboratorio();
+        return p.getNinosEsperandoVuelta();
+    }
+
+    @Override
+    public int getNinosPortalVueltaCentroComercial() throws RemoteException {
+        Portal p = backend.getHawkins().getPortalCentroComercial();
+        return p.getNinosEsperandoVuelta();
+    }
+
+    @Override
+    public int getNinosPortalVueltaBosque() throws RemoteException {
+        Portal p = backend.getHawkins().getPortalBosque();
+        return p.getNinosEsperandoVuelta();
+    }
+
+    @Override
+    public int getNinosPortalVueltaAlcantarillado() throws RemoteException {
+        Portal p = backend.getHawkins().getPortalAlcantarillado();
+        return p.getNinosEsperandoVuelta();
+    }
+
+    @Override
+    public List<Demogorgon> getDemogorgonsLaboratorio() throws RemoteException {
+        return backend.getUpsideDown().getLaboratorio().getListaDemogorgons();
+    }
+
+    @Override
+    public List<Demogorgon> getDemogorgonsCentroComercial() throws RemoteException {
+        return backend.getUpsideDown().getCentroComercial().getListaDemogorgons();
+    }
+
+    @Override
+    public List<Demogorgon> getDemogorgonsBosque() throws RemoteException {
+        return backend.getUpsideDown().getBosque().getListaDemogorgons();
+    }
+
+    @Override
+    public List<Demogorgon> getDemogorgonsAlcantarillado() throws RemoteException {
+        return backend.getUpsideDown().getAlcantarillado().getListaDemogorgons();
+    }
+
+    @Override
+    public List<Nino> getNinosCallePrincipal() throws RemoteException {
+        return backend.getHawkins().getCallePrincipal().getListaNinos();
+    }
+
+    @Override
+    public List<Nino> getNinosSotanoByers() throws RemoteException {
+        return backend.getHawkins().getSotanoByers().getListaNinos();
+    }
+
+    @Override
+    public List<Nino> getNinosRadioWSQK() throws RemoteException {
+        return backend.getHawkins().getRadioWSQK().getListaNinos();
     }
 }
